effb7c79ced89c9b7f0a61756e314faf0571e263
Tim Mayberry <mojofunk@gmail.com>
2015-09-15 08:01:28 +1000

    Exclude waf lock files during rsync
    Deleting these files during sync prevents build after configure

--- ardour-build.sh
+++ ardour-build.sh
@@ -92,7 +92,7 @@ mkdir -p $ARDOUR_BUILD_ROOT || exit 1
 
 function sync ()
 {
-	rsync -av --delete --exclude /build $ARDOUR_SRC_DIR/ $CONFIG_BUILD_DIR || exit 1
+	rsync -av --delete --exclude /build --exclude /.lock* $ARDOUR_SRC_DIR/ $CONFIG_BUILD_DIR || exit 1
 }
 
 function configure ()
