34213d72cc94200da7793faf8743d99bb9594e30
Tim Mayberry <mojofunk@gmail.com>
2015-11-23 10:33:58 +1000

    Add more configurations to ardour-build script
    Install into configuration specific install path

--- ardour-build.sh
+++ ardour-build.sh
@@ -14,11 +14,22 @@ ARDOUR_BRANCH=`git rev-parse --abbrev-ref HEAD`
 cd - || exit 1
 
 COMMON_OPTS="--noconfirm --use-external-libs"
-TEST_OPTS="--test --single-tests"
+TESTS="--test --single-tests"
 TEST_BACKENDS="--with-backends=jack,dummy,alsa"
 GTK_DISABLE_DEPRECATED="--gtk-disable-deprecated"
 RELEASE_BACKENDS="--with-backends=jack,dummy,alsa"
 GTK3="--use-gtk3"
+OPTIMIZE="--optimize"
+CLASS_TRACKING="--class-tracking"
+# debug-symols and profile are only relevent for optimized builds as default
+# builds include debug symbols and the default optimization flag(at least for
+# gcc is -O0 that doesn't omit the frame pointer
+DEBUG="--debug-symbols"
+PROFILE="--profile"
+# Puts all symbols(including static) in the dynamic symbol table so generating
+# stack traces at run-time will give a symbol name for static application/GUI
+# symbols rather than an address/offset
+BACKTRACE="--backtrace"
 
 declare -A config
 config["debug"]="$COMMON_OPTS $TEST_BACKENDS"
@@ -26,10 +37,14 @@ config["debug-gtk3"]="$COMMON_OPTS $TEST_BACKENDS $GTK_DISABLE_DEPRECATED $GTK3"
 config["debug-internal-libs"]="--noconfirm"
 config["debug-nojack"]="$COMMON_OPTS --with-backend=dummy,alsa"
 config["debug-gtk-deprecated"]="$COMMON_OPTS $TEST_BACKENDS $GTK_DISABLE_DEPRECATED"
-config["debug-tests"]="$COMMON_OPTS $TEST_OPTS $TEST_BACKENDS"
-config["debug-tests-cxx11"]="$COMMON_OPTS $TEST_OPTS $TEST_BACKENDS --cxx11"
-config["debug-tests-amalgamated"]="$COMMON_OPTS $TEST_OPTS $TEST_BACKENDS --enable-amalgamation"
-config["release"]="$COMMON_OPTS $RELEASE_BACKENDS --optimize"
+config["debug-tests"]="$COMMON_OPTS $TESTS $TEST_BACKENDS"
+config["debug-tests-single"]="$COMMON_OPTS $TESTS $TEST_BACKENDS"
+config["debug-tests-class-tracking"]="$COMMON_OPTS $TESTS $TEST_BACKENDS $BACKTRACE $CLASS_TRACKING"
+config["debug-tests-cxx11"]="$COMMON_OPTS $TESTS $TEST_BACKENDS --cxx11"
+config["debug-tests-amalgamated"]="$COMMON_OPTS $TESTS $TEST_BACKENDS --enable-amalgamation"
+config["release"]="$COMMON_OPTS $RELEASE_BACKENDS $OPTIMIZE"
+config["optimize-debug"]="$COMMON_OPTS $RELEASE_BACKENDS $OPTIMIZE $DEBUG"
+config["optimize-debug-profile"]="$COMMON_OPTS $RELEASE_BACKENDS $OPTIMIZE $DEBUG $PROFILE"
 
 function print_usage ()
 {
@@ -77,6 +92,7 @@ ARDOUR_BUILD_COMMAND="$1"
 ARDOUR_BUILD_CONFIG="$2"
 ARDOUR_BUILD_SCRIPT_PATH=$( cd $(dirname $0) ; pwd -P )
 ARDOUR_BUILD_ROOT="$ARDOUR_BUILD_SCRIPT_PATH/BUILD"
+ARDOUR_INSTALL_ROOT="$ARDOUR_BUILD_SCRIPT_PATH/INSTALL"
 
 ARDOUR_SRC_PATH=$( cd $ARDOUR_SRC_DIR ; pwd -P )
 
@@ -89,8 +105,10 @@ fi
 
 CONFIG_BUILD_DIR="$ARDOUR_BUILD_ROOT/$ARDOUR_BRANCH-$ARDOUR_BUILD_CONFIG"
 CONFIG_WAF_BUILD_DIR="$CONFIG_BUILD_DIR/build"
+CONFIG_INSTALL_DIR="$ARDOUR_INSTALL_ROOT/$ARDOUR_BRANCH-$ARDOUR_BUILD_CONFIG"
 
 mkdir -p $ARDOUR_BUILD_ROOT || exit 1
+mkdir -p $ARDOUR_INSTALL_ROOT || exit 1
 
 function sync ()
 {
@@ -115,7 +133,7 @@ function install ()
 {
 	sync
 	cd $CONFIG_BUILD_DIR || exit 1
-	./waf install "$@"
+	./waf install --destdir="$CONFIG_INSTALL_DIR" "$@"
 }
 
 function clean ()
